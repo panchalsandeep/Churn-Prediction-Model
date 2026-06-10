cd7f89b90b6b1700bf4109dde89d0306c9017b9e
Sandeep Panchal <panchalsandeep@gmail.com>
2026-06-10 12:07:51 +0530

    fix: resolve mismatch between local Vite root and Vercel static mapping

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
+  root: 'static',
+  publicDir: '../public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
